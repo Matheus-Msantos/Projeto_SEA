03d4aae109e275e992a26a38f3797c3224a0d61f
Matheus-Msantos <matheusmelosantos10@gmail.com>
2020-11-27 19:54:05 -0300

    UPDATE na tabela interessado

---
+++ login/editar_interessado.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once 'db.php';
+
+if ( isset($_POST['id']) ) {
+    $id = preg_replace('/\D/', '', $_POST['id']);
+
+    $nome = $_POST['nome'];
+    $telefone = $_POST['telefone'];
+    $email = $_POST['email'];
+    $estudouIngles =  $_POST['estudouIngles'];
+    $nota = $_POST['nota'];
+    $imagem = $_POST['MAX_FILE_SIZE'];
+    $mensagem = $_POST['mensagem'];
+    $data = $_POST['data'];
+
+    $objStmt = $db->prepare(" UPDATE Interessado SET nome = :nome, telefone = :telefone, email = :email,  estudouIngles = :estudouIngles, nota = :nota, imagem = :MAX_FILE_SIZE, mensagem = :mensagem, data = :data WHERE id = :id ");
+
+    $objStmt->bindParam(':id', $id);
+    $objStmt->bindParam(':nome', $nome);
+    $objStmt->bindParam(':telefone', $telefone);
+    $objStmt->bindParam(':email', $email);
+    $objStmt->bindParam(':estudouIngles', $estudouIngles);
+    $objStmt->bindParam(':nota', $nota);
+    $objStmt->bindParam(':MAX_FILE_SIZE', $imagem);
+    $objStmt->bindParam(':mensagem', $mensagem);
+    $objStmt->bindParam(':data', $data);
+
+    if ( $objStmt->execute() ) {
+        $msg = 'Dados Atualizados!';  
+    }else {
+        $msg = 'ERRO ao atualizar os dados!';  
+    }
+}
+
+$_GET['id'] = $_GET['id'] ?? $_POST['id'] ?? null;
+
+//Evita SQL injection
+$id = preg_replace('/\D/', '', $_GET['id']);
+
+$interessado = array();
+
+$consulta = (" SELECT id, nome, telefone, email, estudouIngles, quantoTempo, nota, imagem, mensagem, data FROM Interessado WHERE id = $id");
+
+foreach ( $db->query( $consulta ) as $registro ) {
+    $interessado = $registro;
+}
+
+
+include 'editar_interessado_tpl.php';
