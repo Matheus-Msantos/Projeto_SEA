25446cc57988a43dfeec36b2e17c7b05265616b6
Matheus-Msantos <matheusmelosantos10@gmail.com>
2020-11-27 19:56:33 -0300

    Mensagem de cadastro

---
+++ login/grava_interessado_tpl.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1> 
+        <?php
+            echo $msg;
+        ?> 
+    </h1>
+</body>
+</html>
